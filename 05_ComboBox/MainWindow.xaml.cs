1d2a5f4d3574ed05d4f5bcbf4be0b247a8f03c07
Віталій Лабенський <vitaliy.laben@gmail.com>
2024-03-06 01:37:23 +0200

    ComboBox

---
+++ 05_ComboBox/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _05_ComboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Person> persons = null;
+        public MainWindow()
+        {
+            //INotifyCollectionChanged
+            InitializeComponent();
+            persons = new ObservableCollection<Person>()
+            {
+                new Person(){ Name = "Olga", Surname= "Ivanchuk", Birthday = new DateTime(2000,5,17),},
+                new Person(){ Name = "Bob", Surname= "Tomson", Birthday = new DateTime(2003,8,21),},
+                new Person(){ Name = "Tom", Surname= "Robson", Birthday = new DateTime(2011,3,20),}
+            };
+            comboBox.Items.Clear();
+            //comboBox.SelectedIndex = 0;
+            //foreach (Person person in persons)
+            //{
+            //    comboBox.Items.Add(person);
+            //}
+            //binding colection to comboBox
+            comboBox.ItemsSource = persons;
+            //comboBox.DisplayMemberPath = "FullName";
+            comboBox.DisplayMemberPath = nameof(Person.FullInfoAboutPerson);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var person = new Person() { Name = "New name", Surname = "New Surname", Birthday = new DateTime(2000, 5, 17), };
+            persons.Add(person);
+            MessageBox.Show(persons.Count.ToString());
+            //comboBox.Items.Add(person);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (comboBox.SelectedIndex != -1)
+                persons.RemoveAt(comboBox.SelectedIndex);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            persons.Clear();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(comboBox.SelectedItem != null)
+            {
+                MessageBox.Show(comboBox.SelectedItem.ToString());
+            }
+        }
+    }
+}
---
+++ 05_ComboBox/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ComboBox
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public string FullInfoAboutPerson => Name + " " + Surname;
+        public override string ToString()
+        {
+            return Name + " " + Surname + " " + Birthday;
+        }
+    }
+}
