17a46d0af48a505f46be50c029e70a563477d13f
Віталій Лабенський <vitaliy.laben@gmail.com>
2024-03-06 03:06:40 +0200

    ListBox

---
+++ 06_ListBox/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _06_ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Student> students;
+        public MainWindow()
+        {
+            InitializeComponent();
+            students = new ObservableCollection<Student>()
+            {
+                 new Student(){Name="Bob", Age = 25},
+                 new Student(){Name="Will", Age = 33},
+                 new Student(){Name="Tom", Age = 15},
+                 new Student(){Name="Jack", Age = 45}
+            };
+            list.Items.Clear();
+            list.ItemsSource = students;
+            list.DisplayMemberPath = nameof(Student.FullInfo);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            students.Add(new Student() { Name = "Name", Age = 100 });
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedItem != null)
+                students.Remove((list.SelectedItem as Student)!);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedItem != null)
+            {
+                Student st = (list.SelectedItem as Student)!;
+                st.Name = "New Name";
+                st.Age = 12;
+                //MessageBox.Show(st.Name + " " + st.Age);
+            }
+        }
+    }
+}
---
+++ 06_ListBox/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ListBox
+{
+    internal class Student : INotifyPropertyChanged
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                name = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullInfo));
+            }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            { 
+                age = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullInfo));
+            }
+        }
+        public string FullInfo => Name + " " + Age;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}  {Age}";
+        }
+    }
+}
